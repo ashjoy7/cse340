20c3f803509e1bad7f481ac9ed770aa9fa6bfd6b
ashjoy7 <ashjoy7@gmail.com>
2024-06-05 00:13:30 -0700

    update

--- server.js
+++ server.js
@@ -5,34 +5,36 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const staticRoute = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const inventoryRoute = require("./routes/inventoryRoute"); // Add this line
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
-app.use(static)
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
+app.use(staticRoute);
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT;
+const host = process.env.HOST;
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
 app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+  console.log(`app listening on ${host}:${port}`);
+});
+
 // Index route
 app.get("/", (req, res) => {
   baseController.buildHome(req, res);
@@ -40,10 +42,10 @@ app.get("/", (req, res) => {
 });
 
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
 
-//Static files
-app.use(express.static('public'))
+// Static files
+app.use(express.static('public'));
 
 // Serve CSS files from the public/css folder
 const directoryPath = __dirname;
