1e4626b2a3ce01c3d4250562acc83ce1deabd59c
ashjoy7 <ashjoy7@gmail.com>
2024-06-13 01:43:31 -0700

    update

--- utilities/index.js
+++ utilities/index.js
@@ -52,7 +52,7 @@ Util.buildClassificationGrid = async function (data) {
         vehicle.inv_make +
         " " +
         vehicle.inv_model +
-        ' details"><img src="/images/vehicles/' +
+        ' details"><img src="public/images/vehicles/' +
         vehicle.inv_thumbnail +
         '" alt="Image of ' +
         vehicle.inv_make +
