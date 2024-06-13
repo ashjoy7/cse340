bbbc2c9697d3a6c739ecdf28f8c20616699ceffe
ashjoy7 <ashjoy7@gmail.com>
2024-06-13 01:11:23 -0700

    update

--- utilities/index.js
+++ utilities/index.js
@@ -21,50 +21,72 @@ Util.getNav = async function () {
  * Constructs the vehicle detail HTML
  ************************** */
 Util.buildVehicleDetail = function (vehicle) {
-  let detailHtml = `<div class="vehicle-detail">
-                      <img src="/images/vehicles/${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
-                      <div class="vehicle-info">
-                        <h2>${vehicle.inv_make} ${vehicle.inv_model}</h2>
-                        <p>Year: ${vehicle.inv_year}</p>
-                        <p>Price: $${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</p>
-                        <p>Mileage: ${new Intl.NumberFormat('en-US').format(vehicle.inv_miles)} miles</p>
-                        <p>Description: ${vehicle.inv_description}</p>
-                      </div>
-                    </div>`;
+  let detailHtml = `
+    <div class="vehicle-detail">
+      <img class="vehicle-image" src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
+      <div class="vehicle-info">
+        <h1>${vehicle.inv_make} ${vehicle.inv_model}</h1>
+        <p><strong>Year:</strong> ${vehicle.inv_year}</p>
+        <p><strong>Price:</strong> $${new Intl.NumberFormat('en-US').format(vehicle.inv_price)}</p>
+        <p><strong>Mileage:</strong> ${new Intl.NumberFormat('en-US').format(vehicle.inv_miles)} miles</p>
+        <p><strong>Description:</strong> ${vehicle.inv_description}</p>
+        <p><strong>Color:</strong> ${vehicle.inv_color}</p>
+      </div>
+    </div>`;
   return detailHtml;
-}
+};
 
 /* **************************************
-* Build the classification view HTML
-* ************************************ */
-Util.buildClassificationGrid = async function(data){
+ * Build the classification view HTML
+ * ************************************ */
+Util.buildClassificationGrid = async function (data) {
   let grid;
   if (data.length > 0) {
     grid = '<ul id="inv-display">';
-    data.forEach(vehicle => { 
+    data.forEach((vehicle) => {
       grid += '<li>';
-      grid +=  '<a href="/inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + ' details"><img src="/images/vehicles/' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>';
+      grid +=
+        '<a href="/inv/detail/' +
+        vehicle.inv_id +
+        '" title="View ' +
+        vehicle.inv_make +
+        " " +
+        vehicle.inv_model +
+        ' details"><img src="' +
+        vehicle.inv_image +
+        '" alt="Image of ' +
+        vehicle.inv_make +
+        " " +
+        vehicle.inv_model +
+        ' on CSE Motors" /></a>';
       grid += '<div class="namePrice">';
-      grid += '<hr />';
-      grid += '<h2>';
-      grid += '<a href="/inv/detail/' + vehicle.inv_id +'" title="View ' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
-      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>';
-      grid += '</h2>';
-      grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>';
-      grid += '</div>';
-      grid += '</li>';
+      grid += "<hr />";
+      grid += "<h2>";
+      grid +=
+        '<a href="/inv/detail/' +
+        vehicle.inv_id +
+        '" title="View ' +
+        vehicle.inv_make +
+        " " +
+        vehicle.inv_model +
+        ' details">' +
+        vehicle.inv_make +
+        " " +
+        vehicle.inv_model +
+        "</a>";
+      grid += "</h2>";
+      grid +=
+        "<span>$" +
+        new Intl.NumberFormat("en-US").format(vehicle.inv_price) +
+        "</span>";
+      grid += "</div>";
+      grid += "</li>";
     });
-    grid += '</ul>';
-  } else { 
+    grid += "</ul>";
+  } else {
     grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>';
   }
   return grid;
-}
+};
 
 module.exports = Util;
