17ff9012c797235f7958df7f5525ff385303157e
ashjoy7 <ashjoy7@gmail.com>
2024-06-20 14:36:53 -0700

    update

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,14 +4,14 @@ const baseController = {};
 
 baseController.buildHome = async function(req, res) {
   try {
-    // Example: Fetch navigation data asynchronously
     const nav = await utilities.getNav();
+    req.flash("notice", "This is a flash message."); // Set the flash message
 
-    // Set a flash message
-    req.flash("notice", "This is a flash message.");
-
+    // Fetch all types of flash messages and pass to views
+    const messages = req.flash();
+    
     // Render the view with data
-    res.render("index", { title: "Home", nav });
+    res.render("index", { title: "Home", nav, messages });
   } catch (err) {
     console.error(err);
     res.status(500).send('500: Internal Server Error');
