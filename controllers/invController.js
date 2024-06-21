34eb2abf8fa75c1b0730f343a945620c43f4b1bc
ashjoy7 <ashjoy7@gmail.com>
2024-06-21 01:59:59 -0700

    update

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,50 +1,65 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
 
-const invCont = {}
+const invCont = {};
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classification_Id
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  if (!data || data.length === 0) {
-    // Handle the case where no data is returned
-    let nav = await utilities.getNav()
+    const classification_id = req.params.classification_Id;
+    const data = await invModel.getInventoryByClassificationId(classification_id);
+    if (!data || data.length === 0) {
+        // Handle the case where no data is returned
+        let nav = await utilities.getNav();
+        res.render("./inventory/classification", {
+            title: "No vehicles found",
+            nav,
+            grid: '<p class="notice">Sorry, no matching vehicles could be found.</p>',
+        });
+        return;
+    }
+    const grid = await utilities.buildClassificationGrid(data);
+    let nav = await utilities.getNav();
+    const className = data[0].classification_name;
     res.render("./inventory/classification", {
-      title: "No vehicles found",
-      nav,
-      grid: '<p class="notice">Sorry, no matching vehicles could be found.</p>',
-    })
-    return
-  }
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
-}
-
-invCont.buildByInvId = async function (req, res) {
-  const invId = req.params.invId;
-  const vehicle = await invModel.getVehicleById(invId); // Fix the variable name here
-  if (!vehicle) {
-    // Handle the case where no vehicle is found
-    let nav = await utilities.getNav()
-    res.render("./inventory/detail", {
-      title: "Vehicle not found",
-      nav,
-      detailHtml: '<p class="notice">Sorry, the vehicle could not be found.</p>',
-    })
-    return
-  }
-  const nav = await utilities.getNav();
-  const detailHtml = await utilities.buildVehicleDetail(vehicle);
-  res.render("inventory/detail", { title: `${vehicle.inv_make} ${vehicle.inv_model}`, nav, detailHtml });
+        title: className + " vehicles",
+        nav,
+        grid,
+    });
 };
 
-module.exports = invCont
+/* ***************************
+ *  Build inventory detail view
+ * ************************** */
+invCont.buildByInvId = async function (req, res) {
+    const invId = req.params.invId;
+    const vehicle = await invModel.getVehicleById(invId);
+    if (!vehicle) {
+        // Handle the case where no vehicle is found
+        let nav = await utilities.getNav();
+        res.render("./inventory/detail", {
+            title: "Vehicle not found",
+            nav,
+            detailHtml: '<p class="notice">Sorry, the vehicle could not be found.</p>',
+        });
+        return;
+    }
+    const nav = await utilities.getNav();
+    const detailHtml = await utilities.buildVehicleDetail(vehicle);
+    res.render("inventory/detail", { title: `${vehicle.inv_make} ${vehicle.inv_model}`, nav, detailHtml });
+};
+
+/* ***************************
+ *  Build inventory management view
+ * ************************** */
+invCont.buildManagementView = async function (req, res, next) {
+    let nav = await utilities.getNav();
+    res.render("./inventory/management", {
+        title: "Inventory Management",
+        nav,
+        messages: req.flash('notice')
+    });
+};
+
+module.exports = invCont;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,4 +1,3 @@
-// Needed Resources 
 const express = require("express");
 const router = new express.Router();
 const invController = require("../controllers/invController");
@@ -8,5 +7,8 @@ router.get("/type/:classification_Id", invController.buildByClassificationId);
 
 // Route to get vehicle details by ID
 router.get("/detail/:inv_Id", invController.buildByInvId);
+
+// Route to build inventory management view
+router.get("/", invController.buildManagementView);
 
 module.exports = router;
