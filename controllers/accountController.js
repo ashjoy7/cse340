b91eead5426be93c6ce82059f66d7a366e148c1a
ashjoy7 <ashjoy7@gmail.com>
2024-06-20 20:41:42 -0700

    update

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -60,36 +60,39 @@ async function buildRegister(req, res, next) {
 /* ****************************************
 *  Process Registration
 * *************************************** */
-async function registerAccount(req, res) {
-  let nav = await utilities.getNav()
-  const { account_firstname, account_lastname, account_email, account_password } = req.body
+async function registerAccount(req, res, next) {
+  let nav = await utilities.getNav();
+  const { account_firstname, account_lastname, account_email, account_password } = req.body;
 
-  const regResult = await accountModel.registerAccount(
-    account_firstname,
-    account_lastname,
-    account_email,
-    account_password
-  )
+  try {
+    const regResult = await accountModel.registerAccount(
+      account_firstname,
+      account_lastname,
+      account_email,
+      account_password
+    );
 
-  if (regResult) {
-    req.flash(
-      "notice",
-      `Congratulations, you\'re registered ${account_firstname}. Please log in.`
-    )
-    res.status(201).render("account/login", {
-      title: "Login",
-      nav,
-    })
-  } else {
-    req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
-      title: "Registration",
-      nav,
-    })
+    if (regResult) {
+      req.flash(
+        "notice",
+        `Congratulations, you\'re registered ${account_firstname}. Please log in.`
+      );
+      res.status(201).render("account/login", {
+        title: "Login",
+        nav,
+      });
+    } else {
+      req.flash("notice", "Sorry, the registration failed.");
+      res.status(501).render("account/register", {
+        title: "Registration",
+        nav,
+      });
+    }
+  } catch (error) {
+    next(error); // Pass the error to Express error handler
   }
 }
 
-// Export all functions
 module.exports = { 
   buildLogin, 
   processLogin, 
--- models/account-model.js
+++ models/account-model.js
@@ -1,11 +1,18 @@
+const pool = require("../database/");
+
 /* *****************************
-*   Register new account
-* *************************** */
+ *   Register new account
+ * *************************** */
 async function registerAccount(account_firstname, account_lastname, account_email, account_password){
     try {
-      const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *"
-      return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
+      const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *";
+      const result = await pool.query(sql, [account_firstname, account_lastname, account_email, account_password]);
+      return result.rows[0]; // Assuming you want to return the inserted row
     } catch (error) {
-      return error.message
+      throw error; // Throw the error to be caught by the caller
     }
-  }
+}
+
+module.exports = {
+  registerAccount
+};
