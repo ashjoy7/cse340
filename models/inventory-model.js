6c987aacae352e6928b8866f50b8d3bf4bc32cf9
ashjoy7 <ashjoy7@gmail.com>
2024-06-21 02:24:19 -0700

    update

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -4,7 +4,13 @@ const pool = require("../database/");
  *  Get all classifications
  * ************************** */
 async function getClassifications() {
-  return pool.query("SELECT * FROM public.classification ORDER BY classification_name");
+  try {
+    const data = await pool.query("SELECT * FROM public.classification ORDER BY classification_name");
+    return data.rows;
+  } catch (error) {
+    console.error("getClassifications error " + error);
+    return [];
+  }
 }
 
 /* ***************************
@@ -32,8 +38,40 @@ async function getVehicleById(invId) {
   }
 }
 
+/* ***************************
+ *  Add new classification
+ * ************************** */
+async function addNewClassification(classification_name) {
+  try {
+    const query = "INSERT INTO public.classification (classification_name) VALUES ($1)";
+    await pool.query(query, [classification_name]);
+    return true;
+  } catch (error) {
+    console.error("addNewClassification error " + error);
+    return false;
+  }
+}
+
+/* ***************************
+ *  Add new inventory item
+ * ************************** */
+async function addNewInventoryItem(inv_make, inv_model, inv_year, inv_price, classification_id, inv_image_path, inv_thumbnail_path) {
+  try {
+    const query =
+      "INSERT INTO public.inventory (inv_make, inv_model, inv_year, inv_price, classification_id, inv_image_path, inv_thumbnail_path) " +
+      "VALUES ($1, $2, $3, $4, $5, $6, $7)";
+    await pool.query(query, [inv_make, inv_model, inv_year, inv_price, classification_id, inv_image_path, inv_thumbnail_path]);
+    return true;
+  } catch (error) {
+    console.error("addNewInventoryItem error " + error);
+    return false;
+  }
+}
+
 module.exports = {
   getClassifications,
   getInventoryByClassificationId,
-  getVehicleById
+  getVehicleById,
+  addNewClassification,
+  addNewInventoryItem
 };
